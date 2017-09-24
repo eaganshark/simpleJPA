a897a9dc50315365cec4f0bdb3014fbb4fe58155
sean <me@example.com>
2017-09-24 09:50:05 -0500

    Testing second commit

--- simpleJPA/src/app/TestJPAApp.java
+++ simpleJPA/src/app/TestJPAApp.java
@@ -12,6 +12,7 @@ public class TestJPAApp {
 	public static void main(String[] args) {
 		
 		// Testing GIT check-in
+		// Testing GIT check-in second test
 		System.out.println("Beginning TestJPAApp Program...");
 		
 		if (args.length != 1) {
